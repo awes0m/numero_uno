2737bfec5a5163495e6026cb11fa3815a0618621
Som <somsubhra.pandit@gmail.com>
2025-07-26 18:21:16 +0530

    Add url_launcher package and implement link in AppFooter; update generated files for platform support

--- lib/views/widgets/app_footer.dart
+++ lib/views/widgets/app_footer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../config/app_theme.dart';
 import '../../utils/responsive_utils.dart';
 
@@ -13,19 +14,29 @@ class AppFooter extends StatelessWidget {
         vertical: ResponsiveUtils.getSpacing(context, AppTheme.spacing16),
         horizontal: ResponsiveUtils.getSpacing(context, AppTheme.spacing24),
       ),
-      child: Text(
-        'Made with ❤️ by awes0m',
-        style: Theme.of(context).textTheme.bodySmall?.copyWith(
-          fontSize: ResponsiveUtils.responsiveValue(
-            context: context,
-            mobile: 12.0,
-            tablet: 13.0,
-            desktop: 14.0,
+      child: InkWell(
+        onTap: () async {
+          final uri = Uri.parse('https://awes0m.github.io/');
+          if (!await launchUrl(uri)) {
+            throw Exception('Could not launch $uri');
+          }
+        },
+        child: Text(
+          'Made with ❤️ by awes0m',
+          style: Theme.of(context).textTheme.bodySmall?.copyWith(
+            fontSize: ResponsiveUtils.responsiveValue(
+              context: context,
+              mobile: 12.0,
+              tablet: 13.0,
+              desktop: 14.0,
+            ),
+            color: Theme.of(
+              context,
+            ).textTheme.bodySmall?.color?.withOpacity(0.7),
+            fontWeight: FontWeight.w400,
           ),
-          color: Theme.of(context).textTheme.bodySmall?.color?.withOpacity(0.7),
-          fontWeight: FontWeight.w400,
+          textAlign: TextAlign.center,
         ),
-        textAlign: TextAlign.center,
       ),
     );
   }
